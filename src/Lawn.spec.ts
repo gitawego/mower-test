b0faaad5b9e82a4b7cf947bd3f478e5bda2f0eff
Hongbo LU <gitawego@gmail.com>
2017-01-14 15:04:45 +0100

    test(mower): added more test

--- src/Lawn.spec.ts
+++ src/Lawn.spec.ts
@@ -1,13 +1,11 @@
 import { Lawn } from './Lawn';
+import { Mower } from './Mower';
 import * as fs from 'fs';
 
 describe('Lawn', () => {
 	const script = fs.readFileSync(`${process.cwd()}/test/script.txt`, 'utf8');
 	it('should run two mowers with final results: 1 3 N and 5 1 E', () => {
 		const lawn = Lawn.schedule(script);
-		lawn.on('change', (data) => {
-			console.log('data', data);
-		});
 		lawn.run();
 		expect(lawn.mowers.length).toEqual(2);
 		expect(lawn.mowers[0].getPosition()).toEqual({
@@ -20,5 +18,61 @@ describe('Lawn', () => {
 			y: 1,
 			d: 'E'
 		});
+	});
+	it('should run two mowers programmatically', () => {
+		const lawn = new Lawn({
+			x: 5,
+			y: 5
+		});
+		const m1 = lawn.addMower({
+			x: 1,
+			y: 2,
+			d: 'N'
+		});
+		m1.command('GAGAGAGAA');
+		const m2 = lawn.addMower({
+			x: 3,
+			y: 3,
+			d: 'E'
+		});
+		m2.command('AADAADADDA');
+		expect(m1.getPosition()).toEqual({
+			x: 1,
+			y: 3,
+			d: 'N'
+		});
+		expect(m2.getPosition()).toEqual({
+			x: 5,
+			y: 1,
+			d: 'E'
+		});
+	});
+	it('should receive event about movement', (done) => {
+		const lawn = new Lawn({
+			x: 5,
+			y: 5
+		});
+		const movements = [];
+		const m1 = lawn.addMower({
+			x: 1,
+			y: 2,
+			d: 'N'
+		});
+		lawn.on('change', (evt) => {
+			expect(evt.mower).toEqual(m1);
+			movements.push(evt.mower.getPosition());
+		});
+
+		m1.command('GAGAGAGAA');
+		expect(movements).toEqual([{ x: 1, y: 2, d: 'W' },
+		{ x: 0, y: 2, d: 'W' },
+		{ x: 0, y: 2, d: 'S' },
+		{ x: 0, y: 1, d: 'S' },
+		{ x: 0, y: 1, d: 'E' },
+		{ x: 1, y: 1, d: 'E' },
+		{ x: 1, y: 1, d: 'N' },
+		{ x: 1, y: 2, d: 'N' },
+		{ x: 1, y: 3, d: 'N' }]);
+		done();
 	});
 })
